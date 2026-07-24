0a5ece2d046059e59b1c53ddfc6bb7d861fc3491
Kaley Scott <kaleysco@gmail.com>
2026-07-24 01:08:30 +0100

    Create Guests management page layout

---
+++ HotelManagement.WPF/Data/Entities/Guest.cs
@@ -0,0 +1,25 @@
+﻿namespace HotelManagement.WPF.Data.Entities
+{
+    public class Guest
+    {
+        public int GuestId { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneContact { get; set; }
+
+        public string Email { get; set; }
+
+        public string DocumentType { get; set; }   // Passport, Driver's License, National ID
+
+        public string DocumentNumber { get; set; }
+
+        /// <summary>
+        /// Returns guest ID and name as a string.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{GuestId} - {Name}";
+        }
+    }
+}
--- HotelManagement.WPF/Views/GuestsPage.xaml.cs
+++ HotelManagement.WPF/Views/GuestsPage.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using HotelManagement.WPF.Data.Entities;
+using HotelManagement.WPF.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http; // microsoft's client for communicating with web services
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace HotelManagement.WPF.Views
 {
@@ -20,9 +15,63 @@ namespace HotelManagement.WPF.Views
     /// </summary>
     public partial class GuestsPage : Page
     {
+        private HttpClient client = new HttpClient(); // bridge to API
+
+        private List<Guest> allGuests = new List<Guest>();
+
         public GuestsPage()
         {
             InitializeComponent();
+
+            // Base address of Web API
+            // Every request will start with this URL
+            client.BaseAddress = new Uri("https://localhost:44380/");
+
+            // Load rooms automatically; runs after page finishes loading
+            Loaded += GuestsPage_Loaded;
+        }
+
+
+        private async void GuestsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            HttpResponseMessage response = await client.GetAsync("api/Guests");
+
+            if (response.IsSuccessStatusCode)
+            {
+                allGuests = await response.Content.ReadAsAsync<List<Guest>>();
+
+                dgGuests.ItemsSource = allGuests;
+            }
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddGuest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StayHistory_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
